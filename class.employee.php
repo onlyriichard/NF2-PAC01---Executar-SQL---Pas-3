83f6a4477d329b2a2fcff8c0d69cf32fd4c58402
onlyriichard <55503289+onlyriichard@users.noreply.github.com>
2020-02-07 17:01:12 +0100

    Add files via upload

---
+++ class.employee.php
@@ -0,0 +1,88 @@
+<?php
+require_once ("class.pdofactory.php");
+require_once ("abstract.databoundobject.php");
+
+class Employee extends DataBoundObject {
+
+        protected $USERID;
+        protected $ID;
+        protected $EMP;
+        protected $DES_ID;
+        protected $DP_ID;
+        protected $STAFF;
+
+
+        protected function DefineTableName() {
+                return("employee");
+        }
+
+        protected function DefineRelationMap() {
+                return(array(
+                        "user_id" => "USERID",
+                        "id" => "ID",
+                        "emp_name" => "EMP",
+                        "designation_id" => "DES_ID",
+                        "department_id" => "DP_ID",
+                        "staff_type" => "STAFF"
+                    ));
+        }
+}
+
+$strDSN = "pgsql:dbname=postgres;host=localhost;port=5432;user=postgres;
+                        password=postgres";
+$objPDO = PDOFactory::GetPDO($strDSN, "postgres", "postgres", array());
+
+
+        
+$objPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$objUser = new Employee($objPDO);
+
+$objUser->setUSERID(1);
+$objUser->setEMP("Ricardo");
+$objUser->setDES_ID(1);
+$objUser->setDP_ID(1);
+$objUser->setSTAFF(20);
+
+$user_id = $objUser->getUSERID();
+print "El ID del usuario es: " . $user_id . "<br />";
+
+$employee_id = $objUser->getUSERID();
+print "El ID del employee es: " . $employee_id . "<br />";
+
+print "El Nombre del Empleado es: " . $objUser->getEMP() . "<br />"; 
+print "El ID de designacion es: " . $objUser->getDES_ID() . "<br />"; 
+print "El ID del departamento es: " . $objUser->getDP_ID() . "<br />"; 
+print "El tipo del personal es: " . $objUser->getSTAFF() . "<br />"; 
+
+
+print "Saving...<br />";
+
+$objUser->Save();
+
+$id = $objUser->getID();
+print "ID in database is " . $id . "<br />";
+
+//Destroying object
+print "<br>Destroying object...<br />";
+unset($objUser);
+
+
+//Recreating object
+
+print "<br>Recreating object from ID $id<br />";
+$objUser = new Employee($objPDO, $id);
+
+$objUser->setEMP("Cristian");
+
+
+print "<br>Committing a change.... Richard will become Cristian<br/>";
+
+print "El Nombre del Empleado es: " . $objUser->getEMP() . "<br />"; 
+
+print "Saving...<br />";
+$objUser->Save();
+
+
+
+
+?>
